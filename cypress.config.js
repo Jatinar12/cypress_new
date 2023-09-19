337719d2283a4e05df3a32e2c05c9786cf4bcf76
Danish Soma <danishsoma112@gmail.com>
2023-09-19 16:45:24 +0530

    added allure reporter plugin

--- cypress.config.js
+++ cypress.config.js
@@ -1,29 +1,35 @@
 const { defineConfig } = require("cypress");
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
-const addCucumberPreprocessorPlugin =
-  require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
-const createEsbuildPlugin =
-  require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 
+async function setupNodeEvents(on, config) {
+  // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+  await preprocessor.addCucumberPreprocessorPlugin(on, config);
+
+  on(
+    "file:preprocessor",
+    createBundler({
+      plugins: [createEsbuildPlugin.default(config)],
+    })
+  );
+  allureWriter(on, config);
+
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
+}
 
 module.exports = defineConfig({
   e2e: {
-    async setupNodeEvents(on, config) {
-      const bundler = createBundler({
-        plugins: [createEsbuildPlugin(config)],
-      });
-
-      on("file:preprocessor", bundler);
-      await addCucumberPreprocessorPlugin(on, config);
-
-
-      return config;
-    },
-    specPattern: "cypress/e2e/features/*.feature",
+    setupNodeEvents,   specPattern: "cypress/e2e/features/*.feature",
     baseUrl: "https://admin-demo.nopcommerce.com",
     chromeWebSecurity: false,
     chromeWebSecurity: false,
     viewportHeight: 900,
     viewportWidth: 1440,
+    env: {
+      allureReuseAfterSpec: true,
+    },
   },
 });
